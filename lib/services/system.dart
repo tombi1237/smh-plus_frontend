d7e15371a57f7a681b28a0e02867631fcdf9bb89
commander15 <128611219+commander15@users.noreply.github.com>
2025-09-15 12:48:24 +0100

    Update system.dart

--- lib/services/system.dart
+++ lib/services/system.dart
@@ -12,7 +12,9 @@ class System {
   factory System() => _instance!;
 
   System.init({required String apiUrl})
-    : api = Dio(BaseOptions(baseUrl: apiUrl)) {
+    : api = Dio(BaseOptions(baseUrl: apiUrl, headers: {
+        'Authorization': 'Bearer eyJhbGciOiJIUzM4NCJ9.eyJyb2xlcyI6WyJTSE9QUEVSIl0sIm1vYmlsZSI6IisyMzc2Nzc3Nzc3NzEiLCJ1c2VySWQiOjQsImVtYWlsIjoic2hvcHBlci5zb3BoaWVAc21ocGx1cy5jb20iLCJ1c2VybmFtZSI6InNvcGhpZV9zaG9wcGVyIiwic3ViIjoic29waGllX3Nob3BwZXIiLCJpYXQiOjE3NTc5MzY4NzEsImV4cCI6MTc1ODM2ODg3MX0.7p9d1c2DuKbUq2-osgM0688_AUCvpe26JyHLbRoqymSjacGC2AkiVTO0Gdhq551s',
+      })) {
     _instance = this;
   }
 
